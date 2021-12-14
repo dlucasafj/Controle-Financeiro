b652747ea238ca9ed11a6eb72539cbcc49881728
Daniel Lucas <daniel.lucasafj@gmail.com>
2021-12-14 15:58:57 -0300

    novas categorias adicionadas

--- src/data/category.ts
+++ src/data/category.ts
@@ -4,4 +4,7 @@ export const category:Category = {
     food:{title:'Alimentação',color:'blue',expense:true},
     rent:{title:'Aluguel', color:'brown', expense: true},
     salary: {title:'Salário', color:'green',expense:false},
+    Outros:{title:'Diversos', color:'yellow', expense: true},
+    Conta:{title:'Internet', color:'red', expense: true},
+    
 }
