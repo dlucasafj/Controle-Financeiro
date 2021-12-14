acc60bab6f29411a9a61130610c5d2883959446b
Daniel Lucas <daniel.lucasafj@gmail.com>
2021-12-14 15:58:19 -0300

    Função adicionar item e validação adicionadas

--- src/components/InputArea/index.tsx
+++ src/components/InputArea/index.tsx
@@ -1,21 +1,102 @@
-import { category } from '../../data/category'
-import { Item } from '../../types/item'
-import * as C from './styles'
+import { useState } from "react";
+import { category } from "../../data/category";
+import { Item } from "../../types/item";
+import * as C from "./styles";
 
-type InputAreaProps ={
-    onAdd: (item:Item)=> void
-}
-export const InputArea =({onAdd}:InputAreaProps)=>{
+type InputAreaProps = {
+  onAdd: (item: Item) => void;
+};
+export const InputArea = ({ onAdd }: InputAreaProps) => {
+  const [dateField, setDateField] = useState("");
+  const [titleField, setTitleField] = useState("");
+  const [categoryField, setCategoryField] = useState("");
+  const [valueField, setValueField] = useState(0);
 
-    const handleAddEvent=()=>{
-        // let newItem: Item={
-        //     date:,
-        // }
-        // onAdd(newItem)
+  let categoryKeys: string[] = Object.keys(category);
+
+  const handleAddEvent = () => {
+    // verificação dos Erros
+    let errors: string[] = [];
+
+    if (isNaN(new Date(dateField).getTime())) {
+      errors.push("Data inválida");
     }
-    return (
-        <C.Container>
-            <button onClick={handleAddEvent}></button>
-        </C.Container>
-    )
-}
+    if (!categoryKeys.includes(categoryField)) {
+      errors.push("Categoria Inválida");
+    }
+    if (titleField === "") {
+      errors.push("Titulo vazio");
+    }
+    if (valueField <= 0) {
+      errors.push("Valor inválido");
+    }
+
+    if (errors.length > 0) {
+      alert(errors.join("\n"));
+    } else {
+      //Adiciono novo Item
+      onAdd({
+        date: new Date(dateField),
+        category: categoryField,
+        title: titleField,
+        value: valueField,
+      });
+
+      clearFields();
+    }
+  };
+  const clearFields = () => {
+    setDateField("");
+    setCategoryField("");
+    setTitleField("");
+    setValueField(0);
+  };
+
+  return (
+    <C.Container>
+      <C.InputLabel>
+        <C.InputTitle>Data</C.InputTitle>
+        <C.Input
+          id="date"
+          type="date"
+          value={dateField}
+          onChange={(e) => setDateField(e.target.value)}
+        ></C.Input>
+      </C.InputLabel>
+      <C.InputLabel>
+        <C.InputTitle>Categoria</C.InputTitle>
+        <C.Select
+          value={categoryField}
+          onChange={(e) => setCategoryField(e.target.value)}
+        >
+          <option></option>
+          {categoryKeys.map((key, index) => (
+            <option key={index} value={key}>
+              {category[key].title}
+            </option>
+          ))}
+        </C.Select>
+      </C.InputLabel>
+      <C.InputLabel>
+        <C.InputTitle>Titulo</C.InputTitle>
+        <C.Input
+          type="text"
+          value={titleField}
+          onChange={(e) => setTitleField(e.target.value)}
+        ></C.Input>
+      </C.InputLabel>
+      <C.InputLabel>
+        <C.InputTitle>Valor</C.InputTitle>
+        <C.Input
+          type="number"
+          value={valueField}
+          onChange={(e) => setValueField(parseFloat(e.target.value))}
+        ></C.Input>
+      </C.InputLabel>
+      <C.InputLabel>
+        <C.InputTitle>&nbsp;</C.InputTitle>
+        <C.Button onClick={handleAddEvent}>Adicionar</C.Button>
+      </C.InputLabel>
+    </C.Container>
+  );
+};
